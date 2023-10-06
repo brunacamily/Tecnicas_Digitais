142b363d44894d8d1edfcf1120ffbeaa8eb212ed
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 22:15:29 -0300

    Delete trabalho 1 td/SOMADOR/simulation directory

--- trabalho 1 td/SOMADOR/simulation/qsim/work/somador_vlg_check_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity somador_vlg_check_tst is
-    port(
-        over            : in     vl_logic;
-        sub             : in     vl_logic_vector(3 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end somador_vlg_check_tst;
