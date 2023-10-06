b31975f00a4158dad8a3deb4a159b18c4a112cb4
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 22:16:37 -0300

    Delete trabalho 1 td/bcd-ex3/simulation directory

--- trabalho 1 td/bcd-ex3/simulation/qsim/work/bcd_ex3/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity bcd_ex3 is
-    port(
-        ent             : in     vl_logic_vector(3 downto 0);
-        over            : out    vl_logic;
-        ex3             : out    vl_logic_vector(3 downto 0)
-    );
-end bcd_ex3;
--- trabalho 1 td/bcd-ex3/simulation/qsim/work/bcd_ex3_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity bcd_ex3_vlg_vec_tst is
-end bcd_ex3_vlg_vec_tst;
