3f5dab3a1f1586051fec1bc9d556868c20b8feb4
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 21:25:18 -0300

    Delete exercicios e exemplos aula 5 e 6  TD/exemplo10/simulation directory

--- exercicios e exemplos aula 5 e 6  TD/exemplo10/simulation/qsim/work/exemplo10/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity exemplo10 is
-    port(
-        e               : in     vl_logic_vector(2 downto 0);
-        s               : out    vl_logic_vector(7 downto 0)
-    );
-end exemplo10;
--- exercicios e exemplos aula 5 e 6  TD/exemplo10/simulation/qsim/work/exemplo10_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity exemplo10_vlg_vec_tst is
-end exemplo10_vlg_vec_tst;
