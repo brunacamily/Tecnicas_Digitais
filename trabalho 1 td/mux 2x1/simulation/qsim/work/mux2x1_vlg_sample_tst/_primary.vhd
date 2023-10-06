494a69685260ebae442eae066dd3104606459707
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 22:19:23 -0300

    Delete trabalho 1 td/mux 2x1 directory

--- trabalho 1 td/mux 2x1/simulation/qsim/work/mux2x1_vlg_sample_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity mux2x1_vlg_sample_tst is
-    port(
-        desloca         : in     vl_logic;
-        entrada         : in     vl_logic;
-        sampler_tx      : out    vl_logic
-    );
-end mux2x1_vlg_sample_tst;
