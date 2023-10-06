a9e8c9bf02621ec9d23149f1d32d3caad78b267f
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 21:29:09 -0300

    Delete exercicios e exemplos aula 5 e 6  TD/exemplo3/simulation directory

--- exercicios e exemplos aula 5 e 6  TD/exemplo3/simulation/qsim/work/exemplo3_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity exemplo3_vlg_check_tst is
-    port(
-        s               : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end exemplo3_vlg_check_tst;
--- exercicios e exemplos aula 5 e 6  TD/exemplo3/simulation/qsim/work/exemplo3_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity exemplo3_vlg_vec_tst is
-end exemplo3_vlg_vec_tst;
