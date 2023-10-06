1f979d250683e24946877abd01a7648e8e30d107
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 21:12:37 -0300

    Delete exercicios e exemplos aula 10/17_um contador de 3 bits_com processos/simulation directory

--- exercicios e exemplos aula 10/17_um contador de 3 bits_com processos/simulation/qsim/work/cont_process_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity cont_process_vlg_check_tst is
-    port(
-        q               : in     vl_logic_vector(2 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end cont_process_vlg_check_tst;
--- exercicios e exemplos aula 10/17_um contador de 3 bits_com processos/simulation/qsim/work/cont_process_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity cont_process_vlg_vec_tst is
-end cont_process_vlg_vec_tst;
