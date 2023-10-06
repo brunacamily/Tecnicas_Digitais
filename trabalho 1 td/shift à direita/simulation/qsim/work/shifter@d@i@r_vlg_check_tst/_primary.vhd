6d6710bcb7e5ebffb9d29b0b6c65d5031b767cb6
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 22:11:49 -0300

    Delete trabalho 1 td/shift à direita/simulation directory

--- trabalho 1 td/shift à direita/simulation/qsim/work/shifter@d@i@r_vlg_check_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity shifterDIR_vlg_check_tst is
-    port(
-        over            : in     vl_logic;
-        s               : in     vl_logic_vector(3 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end shifterDIR_vlg_check_tst;
--- trabalho 1 td/shift à direita/simulation/qsim/work/shifter@d@i@r_vlg_sample_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity shifterDIR_vlg_sample_tst is
-    port(
-        in01            : in     vl_logic_vector(3 downto 0);
-        sinalDES        : in     vl_logic;
-        sampler_tx      : out    vl_logic
-    );
-end shifterDIR_vlg_sample_tst;
