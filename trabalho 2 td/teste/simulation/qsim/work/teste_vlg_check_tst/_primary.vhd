d587be2441fee64d1948bd7e1e10ba80c7f58698
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 22:07:18 -0300

    Delete trabalho 2 td/teste directory

--- trabalho 2 td/teste/simulation/qsim/work/teste_vlg_check_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity teste_vlg_check_tst is
-    port(
-        MIN             : in     vl_logic_vector(5 downto 0);
-        SEGs            : in     vl_logic_vector(5 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end teste_vlg_check_tst;
--- trabalho 2 td/teste/simulation/qsim/work/teste_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity teste_vlg_vec_tst is
-end teste_vlg_vec_tst;
