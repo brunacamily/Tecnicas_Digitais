a762dd40c32543b0fc193cc20bd0411af1076a9f
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 21:18:06 -0300

    Delete exercicios e exemplos aula 10/flip-flop D/simulation directory

--- exercicios e exemplos aula 10/flip-flop D/simulation/qsim/work/flip_flop_@d_vlg_check_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity flip_flop_D_vlg_check_tst is
-    port(
-        saida           : in     vl_logic;
-        saida_neg       : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end flip_flop_D_vlg_check_tst;
