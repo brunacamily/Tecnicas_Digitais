acbbca3d84c2ed14e8dc3bf3323dd5abc6875417
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 21:14:06 -0300

    Delete exercicios e exemplos aula 10/ex17 01/simulation directory

--- exercicios e exemplos aula 10/ex17 01/simulation/qsim/work/contador_assin_3_vlg_check_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity contador_assin_3_vlg_check_tst is
-    port(
-        q_not           : in     vl_logic_vector(2 downto 0);
-        q_out           : in     vl_logic_vector(2 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end contador_assin_3_vlg_check_tst;
--- exercicios e exemplos aula 10/ex17 01/simulation/qsim/work/contador_assin_3_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity contador_assin_3_vlg_vec_tst is
-end contador_assin_3_vlg_vec_tst;
