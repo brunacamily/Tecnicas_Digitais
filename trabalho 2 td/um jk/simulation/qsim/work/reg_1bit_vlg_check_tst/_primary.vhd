1fdbfc23d0d5ef8c870cd9ec2f8841b11081a1ae
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 22:07:33 -0300

    Delete trabalho 2 td/um jk directory

--- trabalho 2 td/um jk/simulation/qsim/work/reg_1bit_vlg_check_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity reg_1bit_vlg_check_tst is
-    port(
-        q_not           : in     vl_logic;
-        q_out           : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end reg_1bit_vlg_check_tst;
