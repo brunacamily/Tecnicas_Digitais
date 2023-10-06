6a101a30fc8823e5fa6062bb75db89c86a7d197a
Bruna Camily Domingues Novack <115049373+brunacamily@users.noreply.github.com>
2023-10-05 21:11:59 -0300

    Delete aula pratica 14-04-2023 TD/exemplo15 aula 9/simulation directory

--- aula pratica 14-04-2023 TD/exemplo15 aula 9/simulation/qsim/work/exemplo15_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity exemplo15_vlg_check_tst is
-    port(
-        q               : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end exemplo15_vlg_check_tst;
--- aula pratica 14-04-2023 TD/exemplo15 aula 9/simulation/qsim/work/exemplo15_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity exemplo15_vlg_vec_tst is
-end exemplo15_vlg_vec_tst;
